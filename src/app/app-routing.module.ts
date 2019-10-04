bcd0cb3c57d4d3e05591ed8c6b6634f9109d1019
yishak <yishak1abrham@gmail.com>
2019-10-04 15:47:54 +0300

    added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CartComponent} from './cart/cart.component';
+import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'cart',component:CartComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'login',component:LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
